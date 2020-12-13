8cefa57368b76661061979fa2afd4cc7ddfb35d4
yutatanomoki <yuta3071@gmail.com>
2020-12-13 07:37:00 +0000

    [add]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_085124) do
+ActiveRecord::Schema.define(version: 2020_12_12_075608) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "book_name"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_12_11_085124) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
